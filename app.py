d9df83e8387b95c2efe10334fa4bc1268a024c79
Sanjana Balannagari <Sanjana.Balannagari@gmail.com>
2025-11-15 21:59:16 -0500

    chore: railway deploy config + gunicorn

--- app.py
+++ app.py
@@ -225,4 +225,4 @@ def chat():
     return render_template('chat.html')
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
